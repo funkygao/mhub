b1e6773d76c6ed490ead05761dbd6fa8d8a38311
funkygao <funky.gao@gmail.com>
2014-10-08 16:27:42 +0800

    connect node with mqtt

--- mqtt/route.go
+++ mqtt/route.go
@@ -1,5 +1,10 @@
 package mqtt
 
+import (
+	log "github.com/funkygao/log4go"
+	"net"
+)
+
 var (
 	routings = map[uint8]Handler{
 		CONNECT:    HandleConnect,
@@ -9,3 +14,13 @@ var (
 		DISCONNECT: HandleDisconnect,
 	}
 )
+
+func HandleCommand(mqtt *Mqtt, conn *net.Conn, client **Client) {
+	handler, present := routings[mqtt.FixedHeader.MessageType]
+	if !present {
+		log.Error("unfound command: %v", *mqtt.FixedHeader)
+		return
+	}
+
+	handler(mqtt, conn, client)
+}
--- node/mqtt.go
+++ node/mqtt.go
@@ -1,8 +1,10 @@
 package node
 
 import (
+	"github.com/funkygao/gomqtt/mqtt"
 	log "github.com/funkygao/log4go"
 	"net"
+	"runtime/debug"
 )
 
 func (this *Node) serveMqtt() {
@@ -19,11 +21,44 @@ func (this *Node) serveMqtt() {
 			continue
 		}
 
-		go this.handleConnection(conn.(*net.TCPConn))
+		go this.handleConnection(&conn)
 	}
 }
 
-func (this *Node) handleConnection(conn *net.TCPConn) {
-	log.Debug("conn: %s", conn.RemoteAddr().String())
+func (this *Node) handleConnection(conn *net.Conn) {
+	//log.Debug("got conn: %s", conn.(*net.TCPConn).RemoteAddr().String())
+
+	var client *mqtt.Client = nil
+
+	defer func() {
+		//log.Debug("close conn: %s", conn.RemoteAddr().String())
+		if r := recover(); r != nil {
+			debug.PrintStack()
+		}
+
+		if client != nil {
+			// force client disconnect
+		}
+
+		//conn.Close()
+	}()
+
+	for {
+		// read fixed header of MQTT
+		fixedHeader, body := mqtt.ReadCompleteCommand(conn)
+		if fixedHeader == nil {
+			//log.Error("[%s] nil fixed header", conn.RemoteAddr().String())
+			return
+		}
+
+		parsed, err := mqtt.DecodeAfterFixedHeader(fixedHeader, body)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+
+		mqtt.HandleCommand(parsed, conn, &client)
+
+	}
 
 }
