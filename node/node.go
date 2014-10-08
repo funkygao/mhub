a54bdb8f377af2cf7cf0eae82e7b33a8ab83bdca
funkygao <funky.gao@gmail.com>
2014-10-08 15:28:56 +0800

    logfile and loglevel

--- node/node.go
+++ node/node.go
@@ -5,15 +5,18 @@ import (
 )
 
 type Node struct {
-	cf *config.Config
+	cf       *config.Config
+	stopChan chan bool
 }
 
 func New(cf *config.Config) (this *Node) {
 	this = new(Node)
 	this.cf = cf
+	this.stopChan = make(chan bool)
 	return
 }
 
 func (this *Node) ServeForever() {
+	<-this.stopChan
 
 }
