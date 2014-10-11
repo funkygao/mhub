81efbd6157a973328c3cdef8a99621d3e90c5de3
funkygao <funky.gao@gmail.com>
2014-10-11 16:32:35 +0800

    stats include cumulated sessions count

--- broker/stats.go
+++ broker/stats.go
@@ -14,25 +14,32 @@ import (
 type stats struct {
 	interval time.Duration
 
-	topics  int64 // TODO
-	recv    int64
-	sent    int64
-	clients int64
-	peers   int64
+	topics   int64 // TODO
+	recv     int64
+	sent     int64
+	sessions int64 // cumulated
+	clients  int64
+	peers    int64
 }
 
-func (this *stats) messageRecv()      { atomic.AddInt64(&this.recv, 1) }
-func (this *stats) messageSend()      { atomic.AddInt64(&this.sent, 1) }
-func (this *stats) clientConnect()    { atomic.AddInt64(&this.clients, 1) }
+func (this *stats) messageRecv() { atomic.AddInt64(&this.recv, 1) }
+func (this *stats) messageSend() { atomic.AddInt64(&this.sent, 1) }
+func (this *stats) clientConnect() {
+	atomic.AddInt64(&this.clients, 1)
+	atomic.AddInt64(&this.sessions, 1)
+}
 func (this *stats) clientDisconnect() { atomic.AddInt64(&this.clients, -1) }
 func (this *stats) peerConnect()      { atomic.AddInt64(&this.peers, 1) }
 func (this *stats) peerDisconnect()   { atomic.AddInt64(&this.peers, -1) }
 
 func (this *stats) String() string {
-	return fmt.Sprintf("{topic:%d, recv:%d, sent:%d, client:%d, peer:%d}",
+	return fmt.Sprintf("{topic:%d, recv:%d, sent:%d, sess:%d, client:%d, peer:%d}",
 		atomic.LoadInt64(&this.topics),
-		atomic.LoadInt64(&this.recv), atomic.LoadInt64(&this.sent),
-		atomic.LoadInt64(&this.clients), atomic.LoadInt64(&this.peers))
+		atomic.LoadInt64(&this.recv),
+		atomic.LoadInt64(&this.sent),
+		atomic.LoadInt64(&this.sessions),
+		atomic.LoadInt64(&this.clients),
+		atomic.LoadInt64(&this.peers))
 }
 
 // current simultaneous client conns
@@ -67,7 +74,7 @@ func (this *stats) start() {
 		lastUserTime = userTime
 		lastSysTime = sysTime
 
-		log.Info("stats:%s, ver:%s, goroutine:%d, mem:%s, cpu:{%3.2f%%us, %3.2f%%sy}",
+		log.Info("%s, ver:%s, goroutine:%d, mem:%s, cpu:{%3.2f%%us, %3.2f%%sy}",
 			this,
 			server.BuildID,
 			runtime.NumGoroutine(),
