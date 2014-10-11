4db22660734c4ace76f8d591ffde2936eaff0dd6
funkygao <funky.gao@gmail.com>
2014-10-11 22:17:25 +0800

    add redis config parser

--- config/config.go
+++ config/config.go
@@ -9,21 +9,28 @@ import (
 type Config struct {
 	Broker BrokerConfig
 	Peers  PeersConfig
+	Redis  RedisConfig
 }
 
 func LoadConfig(cf *conf.Conf) *Config {
 	this := new(Config)
+
 	section, err := cf.Section("broker")
-	if err != nil {
-		panic(err)
+	if err == nil {
+		this.Broker = BrokerConfig{}
+		this.Broker.loadConfig(section)
 	}
-	this.Broker = BrokerConfig{}
-	this.Broker.loadConfig(section)
 
 	section, err = cf.Section("peers")
 	if err == nil {
 		this.Peers = PeersConfig{}
 		this.Peers.loadConfig(section)
+	}
+
+	section, err = cf.Section("redis")
+	if err == nil {
+		this.Redis = RedisConfig{}
+		this.Redis.loadConfig(section)
 	}
 
 	log.Info("Config: %+v", *this)
---
+++ config/redis.go
@@ -0,0 +1,12 @@
+package config
+
+import (
+	conf "github.com/funkygao/jsconf"
+)
+
+type RedisConfig struct {
+}
+
+func (this *RedisConfig) loadConfig(cf *conf.Conf) {
+
+}
