83237f89f9e7837f43a0f1016b335c8434171ebb
funkygao <funky.gao@gmail.com>
2014-10-11 11:50:31 +0800

    a better payload

--- cmd/many.go
+++ cmd/many.go
@@ -17,6 +17,7 @@ import (
 	proto "github.com/funkygao/mqttmsg"
 )
 
+var broadcast = flag.Bool("broadcast", false, "each message will be broadcast to each client")
 var conns = flag.Int("conns", 10, "how many conns (0 means infinite)")
 var host = flag.String("host", "localhost:1883", "hostname of broker")
 var user = flag.String("user", "", "username")
@@ -34,7 +35,7 @@ func main() {
 
 	if flag.NArg() != 2 {
 		topic = "many"
-		payload = proto.BytesPayload([]byte("hello"))
+		payload = proto.BytesPayload([]byte(`{"payload":{"330":{"uid":53,"march_id":330,"city_id":53,"opp_uid":0,"world_id":1,"type":"encamp","start_x":72,"start_y":64,"end_x":80,"end_y":78,"start_time":1412999095,"end_time":1412999111,"speed":1,"state":"marching","alliance_id":0}}`))
 	} else {
 		topic = flag.Arg(0)
 		payload = proto.BytesPayload([]byte(flag.Arg(1)))
