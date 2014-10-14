6014cabbc074865467954672d726f8f32e30b2ee
funkygao <funky.gao@gmail.com>
2014-10-14 17:48:51 +0800

    benchmark broadcast subs

--- broker/bench_test.go
+++ broker/bench_test.go
@@ -3,6 +3,7 @@ package broker
 import (
 	"sync/atomic"
 	"testing"
+	"time"
 )
 
 func BenchmarkChannelClose(b *testing.B) {
@@ -27,5 +28,35 @@ func BenchmarkAtomicAdd(b *testing.B) {
 	var a int32
 	for i := 0; i < b.N; i++ {
 		atomic.AddInt32(&a, 10)
+	}
+}
+
+func BenchmarkSubscribers(b *testing.B) {
+	b.ReportAllocs()
+
+	cs := make([]*incomingConn, 0)
+	for i := 0; i < 10000; i++ {
+		c := &incomingConn{
+			alive:         true,
+			conn:          nil,
+			jobs:          make(chan job, 10),
+			heartbeatStop: make(chan struct{}),
+			lastOpTime:    time.Now().Unix(),
+		}
+		cs = append(cs, c)
+	}
+	c := &incomingConn{
+		alive:         true,
+		conn:          nil,
+		jobs:          make(chan job, 10),
+		heartbeatStop: make(chan struct{}),
+		lastOpTime:    time.Now().Unix(),
+	}
+	subs := newSubscriptions(5, 10, nil)
+	for _, c = range cs {
+		subs.add("#", c)
+	}
+	for i := 0; i < b.N; i++ {
+		subs.subscribers("ha")
 	}
 }
