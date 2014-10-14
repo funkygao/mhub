6dc6a4d7bb1844bbaee94a2f82e4d64ae6dceb44
funkygao <funky.gao@gmail.com>
2014-10-14 19:39:37 +0800

    flying message status const

--- broker/flight.go
+++ broker/flight.go
@@ -3,8 +3,7 @@ package broker
 type flightMessage struct {
 	Qos          uint8 // the Qos in effect
 	DestClientId string
-	Status       uint8 // The status of this message, like PENDING_PUB(deliver occured
-	// when client if offline), PENDING_ACK, etc
+	Status       uint8 
 	MessageInternalId uint64 // The MqttMessage of interest
 	ClientMessageId   uint16 // The message id to be used in MQTT packet
 }
