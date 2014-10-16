19e814ca49c1adda540bd5fe8d871f8e7ba90462
funkygao <funky.gao@gmail.com>
2014-10-16 22:25:24 +0800

    set initial map size

--- broker/global.go
+++ broker/global.go
@@ -6,7 +6,7 @@ import (
 )
 
 // all clients in mem, key is client id
-var clients map[string]*incomingConn = make(map[string]*incomingConn)
+var clients map[string]*incomingConn = make(map[string]*incomingConn, 10000)
 var clientsMu sync.Mutex
 
 // A random number generator ready to make client-id's, if
